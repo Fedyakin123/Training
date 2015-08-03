4a89cfbf10d24a91da496abaa53a6194b3528e70
novichkovv <novichkovv@bk.ru>
2015-08-04 01:56:02 +0300

    .htacces
    Signed-off-by: novichkovv <novichkovv@bk.ru>

--- www/view.php
+++ www/view.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
-    <html>
-        <head>
-            <title>
-                Users Authorization
-            </title>
-            <meta charset="utf-8">
-        </head>
-        <body>
-            <form method="post" action="">
-                <input type="text" name="user[login]" placeholder="Login"><br />
-                <input type="password" name="user[password]" placeholder="Password"><br />
-                <input type="email" name="user[email]" placeholder="Email" > <br />
-                <input type="text" name="user[name]" placeholder="Name" > <br />
-                <input type="submit" name="save_user_btn" value="Добавить">
-            </form>
-        </body>
-    </html>
+<html>
+    <head>
+        <title>
+            Users Authorization
+        </title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <form method="post" action="">
+            <input type="text" name="user[login]" placeholder="Login"><br />
+            <input type="password" name="user[password]" placeholder="Password"><br />
+            <input type="email" name="user[email]" placeholder="Email" > <br />
+            <input type="text" name="user[name]" placeholder="Name" > <br />
+            <input type="submit" name="save_user_btn" value="Добавить">
+        </form>
+    </body>
+</html>
+
